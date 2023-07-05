d3bfdcf0d6508ba624f10b47cf678f002b332202
Harry <harryberman5@gmail.com>
2023-07-05 14:37:09 -0700

    updated settings and .vs folder

--- Build/WebBuilds.framework.js
+++ Build/WebBuilds.framework.js
@@ -1975,13 +1975,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  3908660: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 3908715: function($0) {performance.now = function() { return $0; };},  
- 3908763: function($0) {performance.now = function() { return $0; };},  
- 3908811: function() {performance.now = Module['emscripten_get_now_backup'];},  
- 3908866: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 3908927: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 3908991: function() {return Module.webglContextAttributes.powerPreference;}
+  3908676: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 3908731: function($0) {performance.now = function() { return $0; };},  
+ 3908779: function($0) {performance.now = function() { return $0; };},  
+ 3908827: function() {performance.now = Module['emscripten_get_now_backup'];},  
+ 3908882: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 3908943: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 3909007: function() {return Module.webglContextAttributes.powerPreference;}
 };
 
 
